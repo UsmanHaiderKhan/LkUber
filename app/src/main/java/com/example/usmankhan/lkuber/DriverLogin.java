7837eb5bd3561d9474c6e8948077064021f63e21
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-30 23:14:57 +0500

    Every thing is pushed on git hub

--- app/src/main/java/com/example/usmankhan/lkuber/DriverLogin.java
+++ app/src/main/java/com/example/usmankhan/lkuber/DriverLogin.java
@@ -36,7 +36,7 @@ public class DriverLogin extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if (user != null) {
-                    Intent intent = new Intent( DriverLogin.this, MapActivity.class );
+                    Intent intent = new Intent( DriverLogin.this, DriverMap.class );
                     startActivity( intent );
                     finish();
                     return;
